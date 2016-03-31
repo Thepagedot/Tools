285fad869c74ca7a29e655e7c4781c491e344384
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2016-03-31 22:39:20 +0200

    Added Xamarin.Android Systembar Helper

---
+++ Thepagedot.Tools.Xamarin.Android/ActivityHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Views;
+
+namespace Thepagedot.Tools.Xamarin.Android
+{
+	public static class ActivityHelpers
+	{
+		public static void SetSystemBarBackground(this Activity activity, int colorResourceId)
+		{
+			if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop) 
+			{
+				// Set status bar color
+				activity.Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+				activity.Window.SetStatusBarColor (activity.Resources.GetColor (colorResourceId)); //Resource.Color.HomeMaticBlue));
+			}
+		}
+	}
+}
+
