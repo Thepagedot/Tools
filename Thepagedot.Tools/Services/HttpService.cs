9eec0717442f34a37992fcb0d18f29bab1357e5d
Robin-Manuel Thiel <robin-manuel@thiel1.de>
2016-06-01 18:53:44 +0200

    Adjusted HttpService

--- Thepagedot.Tools/Services/HttpService.cs
+++ Thepagedot.Tools/Services/HttpService.cs
@@ -17,20 +17,19 @@ namespace Thepagedot.Tools.Services
 	{
 		// Dictionary for cache files
 		private static readonly Dictionary<string, CacheFile<string>> Cache = new Dictionary<string, CacheFile<string>>();
-
 		// Use the ModernHttpClient to provide a working timeout
 		private static HttpClient HttpClient = new HttpClient(new NativeMessageHandler());
-		//private static CookieContainer Cookies = new CookieContainer();
+        //private static CookieContainer Cookies = new CookieContainer();
+        private readonly TimeSpan _DefaultTimeout = new TimeSpan(0, 0, 60);
 
-
-		/// <summary>
-		/// Returns the response source code of a simple web request
-		/// </summary>
-		/// <returns>The response string.</returns>
-		/// <param name="url">Request URL.</param>
-		/// <param name="cacheTime">Defines how long the cache of this request should be hold.</param>
-		/// <param name="timeout">Defines the timeout.</param>
-		public async Task<string> GetStringAsync(string url, TimeSpan? cacheTime = null, TimeSpan? timeout = null)
+        /// <summary>
+        /// Returns the response source code of a simple web request
+        /// </summary>
+        /// <returns>The response string.</returns>
+        /// <param name="url">Request URL.</param>
+        /// <param name="cacheTime">Defines how long the cache of this request should be hold.</param>
+        /// <param name="timeout">Defines the timeout.</param>
+        public async Task<string> GetStringAsync(string url, TimeSpan? cacheTime = null, TimeSpan? timeout = null)
 		{
 			// Check if cached response is available
 			if (cacheTime != null && Cache.ContainsKey(url))
@@ -44,11 +43,19 @@ namespace Thepagedot.Tools.Services
 				}
 			}
 
-			// Set request timeout if available, otherwise set default timeout
-			if (timeout != null)
-				HttpClient.Timeout = (TimeSpan)timeout;
-			else
-				HttpClient.Timeout = new TimeSpan(0, 0, 60);
+            // Check if Timeout needs to be adjusted
+            if (timeout != null && TimeSpan.Compare((TimeSpan)timeout, _DefaultTimeout) != 0)
+            {
+                // HttpClient Timeout needs to be changed. Unfortunately this leads to creating a new client
+                HttpClient = new HttpClient();
+                HttpClient.Timeout = (TimeSpan)timeout;
+            }
+            else if (TimeSpan.Compare(HttpClient.Timeout, _DefaultTimeout) != 0)
+            {
+                //HttpClient timeout differs from default and is not set in the attributes. Needs to be reseted to default.
+                HttpClient = new HttpClient();
+                HttpClient.Timeout = new TimeSpan(0, 0, 60);
+            }
 
 			try
 			{
@@ -74,7 +81,7 @@ namespace Thepagedot.Tools.Services
 		}
 
 		/// <summary>
-		/// Deletes the chache file for a specific url
+		/// Deletes the cache file for a specific url
 		/// </summary>
 		/// <returns></returns>
 		/// <param name="url">Request URL.</param>
